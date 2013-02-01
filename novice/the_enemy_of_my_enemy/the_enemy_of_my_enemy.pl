8f5406a72428049e3197ca24367d4dd218648abc
wanxiangbin <wanxiangbin@hisense.com>
2013-02-01 16:27:41 +0800

    cpu usage excceed limits, enemy of my enemy

---
+++ novice/the_enemy_of_my_enemy/the_enemy_of_my_enemy.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+#Enter your code here
+sub a{($x,$y)=@_;if($h{$x}) {push @{$h{$x}},$y}else{$h{$x}=[$y]}}
+open($F,"<",shift);while(<$F>){chomp;push @c,$_}
+($n,$p)=split / /,$c[0];map{@e=split;map{a($e[$_],$e[1-$_])}(0..1)}@c[1..$p];
+map{
+ $m=$c[$p+1+$_];
+ $el=$h{$m};
+ @fs=();
+ map{
+  @f = grep {!($_ ~~ @fs)and($_ ne $m)and!($_ ~~ @$el)
+ } @{$h{$_}};
+ push @fs,@f;}@$el;
+ print @fs."\n";
+}(1..$c[$p+1]);
